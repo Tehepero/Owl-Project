42a79f54e87128305555edbdd793252f0e75c51c
Blue <isyuyong6732@naver.com>
2025-01-15 15:16:20 +0900

    첫 생성

---
+++ src/main/java/com/owl/owlproject/OwlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.owl.owlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwlProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/owl/owlproject/OwlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owl.owlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
